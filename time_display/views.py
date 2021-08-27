edfc9009ea7099281606d856760059bf6a0adcec
Jose U <jose.ulloa.herrera@gmail.com>
2021-08-27 19:44:29 -0400

    Se agrega forma de tomar la hora con el modulo datetime y se actualiza index.html

--- time_display/views.py
+++ time_display/views.py
@@ -1,13 +1,19 @@
 # Create your views here.
 from django.shortcuts import render
 from time import gmtime, localtime, strftime
+from datetime import datetime
 
 # Create your views here.
 def index(request):
     context = {
         "time_local_date": strftime("%b %d, %Y", localtime()),
-        "time_local_time": strftime("%H:%M %p", localtime()),
+        "time_local_time": strftime("%H:%M %S", localtime()),
         "time_gm_date": strftime("%b %d, %Y", gmtime()),
-        "time_gm_time": strftime("%H:%M %p", gmtime()),
+        "time_gm_time": strftime("%H:%M %S", gmtime()),
+		"datetime_local_date":datetime.now().strftime("%b %d, %Y"),
+		"datetime_local_time":datetime.now().strftime("%H %M, %S"),
+		"datetime_gm_date":datetime.now().strftime("%b %d, %Y"),
+		"datetime_gm_time":datetime.now().strftime("%H %M, %S"),
+
     }
     return render(request,'index.html', context)
