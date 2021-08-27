a27813ad5e65ad3bc75dc417ed75eab4e542028e
Jose U <jose.ulloa.herrera@gmail.com>
2021-08-27 19:27:09 -0400

    se aplica css y se depura la visualización de los datos

--- time_display/views.py
+++ time_display/views.py
@@ -5,6 +5,9 @@ from time import gmtime, localtime, strftime
 # Create your views here.
 def index(request):
     context = {
-        "time": strftime("%Y-%m-%d %H:%M %p", localtime())
+        "time_local_date": strftime("%b %d, %Y", localtime()),
+        "time_local_time": strftime("%H:%M %p", localtime()),
+        "time_gm_date": strftime("%b %d, %Y", gmtime()),
+        "time_gm_time": strftime("%H:%M %p", gmtime()),
     }
     return render(request,'index.html', context)
