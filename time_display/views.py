33643398a6f116ae709e5d75fe81975e0393f58e
Jose U <jose.ulloa.herrera@gmail.com>
2021-08-27 18:56:46 -0400

    creación de view index para mostrar la hora actual del sistema

--- time_display/views.py
+++ time_display/views.py
@@ -1,6 +1,10 @@
 # Create your views here.
 from django.shortcuts import render
+from time import gmtime, localtime, strftime
 
 # Create your views here.
 def index(request):
-	return render(request, 'index.html')
+    context = {
+        "time": strftime("%Y-%m-%d %H:%M %p", localtime())
+    }
+    return render(request,'index.html', context)
